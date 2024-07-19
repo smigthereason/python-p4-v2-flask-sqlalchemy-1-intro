6c61c79ffcd66c288ad0b5327de092edbf8e6a1d
Victor <victor.dmaina@gmail.com>
2024-07-19 18:10:41 +0300

    complete test

--- server/models.py
+++ server/models.py
@@ -9,10 +9,18 @@ db = SQLAlchemy(metadata=metadata)
 
 # define a model class by inheriting from db.Model.
 
-
 class Pet(db.Model):
     __tablename__ = 'pets'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True,
+                         nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True)
+    verified = db.Column(db.Boolean, default=False)
